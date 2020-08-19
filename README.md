e8548aca193109ce92358acb8de74d875cd41273
Oswld <user.oswld@gmail.com>
2020-08-19 13:43:49 -0500

    1.5 Nuevo flujo de datos y ajustes en formato

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Visio a PDF 
+Script creado en Python para convertir archivos de Visio a PDF por lotes (batch)
+
+## Notas
+```python
+# Ignorar todas las alertas y cuadros de dialogo
+# https://docs.microsoft.com/en-us/office/vba/api/visio.application.alertresponse
+
+visio.AlertResponse = 7
+```
+
+```python
+# Sintaxis de argumentos
+# https://docs.microsoft.com/en-us/office/vba/api/visio.document.exportasfixedformat
+
+document.ExportAsFixedFormat(1, output, 1, 0)
+document.Close()
+```
--- vsd-a-pdf.py
+++ vsd-a-pdf.py
@@ -1,41 +1,63 @@
-import pathlib
-import sys
 import win32com.client
-
-
-def set_ps(page, cell, value):
-    page.PageSheet.Cells(cell).Formula = value
-
-
-def convert(visio, path, out_path):
-    doc = visio.Documents.Open(str(path))
-
-    for page in doc.Pages:
-        set_ps(page, "PageLeftMargin", "0mm")
-        set_ps(page, "PageRightMargin", "0mm")
-        set_ps(page, "PageTopMargin", "0mm")
-        set_ps(page, "PageBottomMargin", "0mm")
-        page.ResizeToFitContents()
-
-    pdf_format = 1
-    intent_print = 1
-    print_all = 0
-    doc.ExportAsFixedFormat(pdf_format, out_path, intent_print, print_all)
-    doc.Close()
+import pathlib
+import shutil
+import sys
+import os
 
 
 def main():
-    visio = win32com.client.Dispatch("Visio.Application")
-    visio.AlertResponse = 7  # Answer "no" to all save dialogs
+    if len(sys.argv) < 2:
+        print("¡¡Es necesario ingresar la ruta de la carpeta como argumento!!")
+        sys.exit(1)
 
-    basedir = pathlib.Path(sys.argv[1])
-    files = list(basedir.glob("*.vsdx"))
+    # Open application
+    visio = win32com.client.Dispatch("Visio.Application")
+
+    visio.AlertResponse = 7
+
+    main_dir = pathlib.Path(sys.argv[1])
+
+    # All MS Visio documents
+    files = list(main_dir.glob("*.vsdx"))
+    print("\n| Total | Archivo")
     for i, path in enumerate(files, start=1):
-        print(f"[{i:3}/{len(files):3}] {path.stem}")
-        out_path = path.with_suffix(path.suffix + ".pdf")
-        if out_path.exists():
+        print(f"| {i} : {len(files)} | {path.stem}")
+        output = path.with_suffix(".pdf")
+        if output.exists():
             continue
-        convert(visio, path, out_path)
+        convert(visio, path, output)
+
+    """
+    # TO-DO
+    main_dir = os.listdir("C:\\Users\\O.Tavares-ext\\Desktop\\VisioAPDF")
+    output_f = "C:\\Users\\O.Tavares-ext\\Desktop\\New folder"
+
+    print("")
+    for files in main_dir:
+        if files.endswith(".pdf"):
+            print("Archivo con exito: " + files)
+            shutil.move(files, output_f)
+
+    print("\nProceso completado")
+    """
+
+
+def convert(visio, path, output):
+    document = visio.Documents.Open(str(path))
+
+    for page in document.Pages:
+        # 0in esta dado por  pulgadas (formato interno)
+        format_visio(page, "PageLeftMargin", "0in")
+        format_visio(page, "PageRightMargin", "0in")
+        format_visio(page, "PageTopMargin", "0in")
+        format_visio(page, "PageBottomMargin", "0in")
+
+    document.ExportAsFixedFormat(1, output, 1, 0)
+    document.Close()
+
+
+def format_visio(page, cell, value):
+    page.PageSheet.Cells(cell).Formula = value
 
 
 if __name__ == "__main__":
