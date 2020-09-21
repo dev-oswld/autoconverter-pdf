da4f313359be667c109a40e9a305a9b48222c3d8
Oswld <user.oswld@gmail.com>
2020-09-21 09:43:48 -0500

    Version final con textos de ayuda (colores en UI)

--- ConvertidorPDF.py
+++ ConvertidorPDF.py
@@ -1,74 +1,85 @@
-from tkinter import messagebox 
+from tkinter import messagebox
 import tkinter.font as tkFont
 import win32com.client
 import tkinter as tk
 import pathlib
 import os
 
-def convertppoint(powerpoint, path, output):
-    document = powerpoint.Presentations.Open(str(path))
-    document.SaveAs(output, 32)  # Parametro de 'formatType'
-    document.Close()
 
 def ppoint():
     # Objeto para la instancia
     powerpoint = win32com.client.Dispatch("Powerpoint.Application")
     main_dir = pathlib.Path(os.getcwd())
 
-    # Cambiar segun el formato
-    # pptx o ppt
+    # Cambiar segun el formato pptx o ppt
     files = list(main_dir.glob("*.pptx"))
-    print("\n| Total | Archivo")
     for i, path in enumerate(files, start=1):
-
-        # Progreso de archivo
-        print(f"| {i} : {len(files)} | {path.stem}")
         output = path.with_suffix(".pdf")
         if output.exists():
             continue
         convertppoint(powerpoint, path, output)
-    # TO-DO
-    messagebox.showinfo("showinfo", "Proceso exitoso") 
+
+    # Fin del proceso
+    mensaje = " " + str(len(files)) + " archivos convertidos"
+    messagebox.showinfo("Proceso completado", mensaje)
+
+
+def convertppoint(powerpoint, path, output):
+    document = powerpoint.Presentations.Open(str(path))
+    document.SaveAs(output, 32)  # Parametro de 'formatType'
+    document.Close()
+
 
 def visio():
-	# Objeto para la instancia
+    # Objeto para la instancia
     visio = win32com.client.Dispatch("Visio.Application")
     visio.AlertResponse = 7
     main_dir = pathlib.Path(os.getcwd())
 
     # Enlista todos los documentos de MS Visio
     files = list(main_dir.glob("*.vsdx"))
-    print("\n| Total | Archivo")
     for i, path in enumerate(files, start=1):
-        print(f"| {i} : {len(files)} | {path.stem}")
         output = path.with_suffix(".pdf")
         if output.exists():
             continue
         convertvisio(visio, path, output)
 
+    # Fin del proceso
+    mensaje = " " + str(len(files)) + " archivos convertidos"
+    messagebox.showinfo("Proceso completado", mensaje)
+
+
 def convertvisio(visio, path, output):
-	document = visio.Documents.Open(str(path))
-	document.ExportAsFixedFormat(1, output, 1, 0)
-	document.Close()
+    document = visio.Documents.Open(str(path))
+    document.ExportAsFixedFormat(1, output, 1, 0)
+    document.Close()
+
 
 def main():
     # Instancias a mostrar
     ventana = tk.Tk()
-    ventana.geometry("450x250")
+    ventana.geometry("450x200")
     ventana.title("Creado por Oswaldo Tavares")
-    estilo = tkFont.Font(family="Arial", size=20)
-
-    txt1 = tk.Label(text="Convertidor de archivos\n", font=estilo, fg="orange")
-
+    estilo = tkFont.Font(family="Arial", size=20, weight="bold")
+    txt1 = tk.Label(text="Convertidor de archivos", font=estilo, fg="orange")
+    txt2 = tk.Label(text="   ")
     # Botones
-    btn_visio = tk.Button(text="Visio a PDF", font=estilo, command=visio)
-    btn_ppoint = tk.Button(text="Power Point a PDF", font=estilo, command=ppoint)
+    btn_visio = tk.Button(
+        text="Visio a PDF", font=estilo, fg="blue", relief="groove", command=visio
+    )
+    txt3 = tk.Label(text="   ")
+    btn_ppoint = tk.Button(
+        text="Power Point a PDF", font=estilo, fg="red", relief="groove", command=ppoint
+    )
 
     # Todo junto para ser mostrado
     txt1.pack()
-    btn_visio.pack() # side=tk.LEFT
-    btn_ppoint.pack() # width=50
+    txt2.pack()
+    btn_visio.pack()
+    txt3.pack()
+    btn_ppoint.pack()
     ventana.mainloop()
+
 
 if __name__ == "__main__":
     main()
